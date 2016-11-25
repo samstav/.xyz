c25346a593b6eb9d71d0fbe582adea61e12604c2
Sam Stavinoha <smlstvnh@gmail.com>
2016-11-25 17:42:33 -0600

    in theory this should work

--- main.tf
+++ main.tf
@@ -2,69 +2,74 @@ provider "aws" {
   region = "${var.aws_region}"
 }
 
+resource "aws_s3_bucket" "tf_remote_config_bucket" {
+  count  = "${var.tf_remote_backend}"
+  bucket = "${var.tf_remote_backend_bucket_prefix}-${var.domain}"
+
+  versioning {
+    enabled = true
+  }
+}
+
+output "tf_config_s3_bucket_id" {
+  value = "${aws_s3_bucket.tf_remote_config_bucket.id}"
+}
+
+output "tf_config_s3_bucket_arn" {
+  value = "${aws_s3_bucket.tf_remote_config_bucket.arn}"
+}
+
+output "tf_config_s3_bucket_region" {
+  value = "${aws_s3_bucket.tf_remote_config_bucket.region}"
+}
+
 provider "mailgun" {
   api_key = "${var.mailgun_api_key}"
 }
 
 resource "mailgun_domain" "this" {
-  name = "${var.domain}"
+  name          = "${var.domain}"
   smtp_password = "${var.mailgun_smtp_password}"
-  spam_action = "${var.mailgun_spam_action}"
-  wildcard = "${var.mailgun_wildcard}"
+  spam_action   = "${var.mailgun_spam_action}"
+  wildcard      = "${var.mailgun_wildcard}"
 }
 
 resource "aws_route53_zone" "this" {
-  name = "${var.domain}"
-  comment = "Domain with mailgun mail managed by terraform."
+  name          = "${var.domain}"
+  comment       = "Domain with mailgun mail managed by terraform."
   force_destroy = true
 }
 
-resource "aws_route53_record" "mailgun_verification_spf" {
+resource "aws_route53_record" "mailgun_sending_records" {
+
+  # This is not allowed (yet), see terraform#1497, hard-coding for now.
+  #  count   = "${length(mailgun_domain.this.sending_records)}"
+  count = 3
+
   zone_id = "${aws_route53_zone.this.zone_id}"
-  name = "${aws_route53_zone.this.name}"
-  type = "TXT"
-  ttl = 300
-  records = ["${var.mailgun_spf_record_value}"]
+  name    = "${element(mailgun_domain.this.sending_records, count.index)}.name"
+  type    = "${element(mailgun_domain.this.sending_records, count.index)}.record_type"
+  ttl     = "${var.record_ttl}"
+  records = ["${element(mailgun_domain.this.sending_records, count.index)}.value"]
 }
 
+resource "aws_route53_record" "mailgun_receiving_records" {
 
-resource "aws_route53_record" "mailgun_verification_spf" {
+  # This is not allowed (yet), see terraform#1497, hard-coding for now.
+  #  count   = "${length(mailgun_domain.this.receiving_records)}"
+  count = 2
+
   zone_id = "${aws_route53_zone.this.zone_id}"
-  name = "${aws_route53_zone.this.name}"
-  type = "TXT"
-  ttl = 300
-  records = ["${var.mailgun_spf_record_value}"]
+  name    = "${element(mailgun_domain.this.receiving_records, count.index)}.name"
+  type    = "${element(mailgun_domain.this.receiving_records, count.index)}.record_type"
+  ttl     = "${var.record_ttl}"
+  records = ["${element(mailgun_domain.this.receiving_records, count.index)}.priority ${element(mailgun_domain.this.receiving_records, count.index)}.value"]
 }
 
-resource "aws_route53_record" "mailgun_verification_dkim" {
-  zone_id = "${aws_route53_zone.this.zone_id}"
-  name = "${var.mailgun_dkim_record_name}"
-  type = "TXT"
-  ttl = 300
-  records = ["${var.mailgun_dkim_record_value}"]
+output "receiving_records" {
+  value = ["${aws_route53_record.mailgun_receiving_records.*.fqdn}"]
 }
 
-resource "aws_route53_record" "mailgun_tracking" {
-  zone_id = "${aws_route53_zone.this.zone_id}"
-  name = "email"
-  type = "CNAME"
-  ttl = 300
-  records = ["mailgun.org"]
+output "sending_records" {
+  value = ["${aws_route53_record.mailgun_sending_records.*.fqdn}"]
 }
-
-resource "aws_route53_record" "mailgun_receiving_mxa" {
-  zone_id = "${aws_route53_zone.this.zone_id}"
-  name = "${aws_route53_zone.this.name}"
-  type = "MX"
-  ttl = 300
-  records = ["10 mxa.mailgun.org"]
-}
-
-resource "aws_route53_record" "mailgun_receiving_mxb" {
-  zone_id = "${aws_route53_zone.this.zone_id}"
-  name = "${aws_route53_zone.this.name}"
-  type = "MX"
-  ttl = 300
-  records = ["10 mxb.mailgun.org"]
-}
-

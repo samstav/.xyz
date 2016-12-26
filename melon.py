d2bed404cc77200d10d001d76968b26586ba1f60
Sam Stavinoha <smlstvnh@gmail.com>
2016-12-25 20:10:27 -0600

    mailgun routes commands

--- melon.py
+++ melon.py
@@ -24,6 +24,39 @@ def _mailgun_session(user, key):
     return mailgun
 
 
+def _show_routes(args):
+    mailgun = _mailgun_session(args.mailgun_user, args.mailgun_api_key)
+    url = '{0}/routes'.format(MAILGUN)
+    res = mailgun.get(url)
+    res.raise_for_status()
+    routes = res.json()['items']
+    print(json.dumps(routes, sort_keys=True, indent=2))
+
+def _create_route(args):
+    mailgun = _mailgun_session(args.mailgun_user, args.mailgun_api_key)
+    url = '{0}/routes'.format(MAILGUN)
+    match_recipient = 'match_recipient("{recipient}@{domain}")'.format(
+        recipient=args.recipient, domain=args.domain)
+    data = [
+        ('expression', match_recipient),
+        ('description', args.description),
+        ('priority', args.priority),
+    ]
+    # Add actions: you can pass multiple 'action' parameters
+    data.extend([('action', 'forward("{0}")'.format(fv))
+                 for fv in args.forward])
+
+    if args.with_stop:
+        # Simply stops the priority waterfall so the subsequent
+        # routes will not be evaluated. Without a stop() action
+        # executed, all lower priority Routes will also be evaluated.
+        data.append(('action', 'stop()'))
+
+    res = mailgun.post(url, data=data)
+    res.raise_for_status()
+    print(json.dumps(res.json(), sort_keys=True, indent=2))
+
+
 def _show_domains(args):
     mailgun = _mailgun_session(args.mailgun_user, args.mailgun_api_key)
     url = '{0}/domains'.format(MAILGUN)
@@ -128,6 +161,7 @@ if __name__ == '__main__':
         )
 
     subparsers = parser.add_subparsers()
+    # `melon show-domains`
     show_domains = subparsers.add_parser(
         'show-domains',
         help='Show domains in Mailgun.'
@@ -135,6 +169,7 @@ if __name__ == '__main__':
     _mailgun_args(show_domains)
     show_domains.set_defaults(_func=_show_domains)
 
+    # `melon show-domain`
     show_domain = subparsers.add_parser(
         'show-domain',
         help='Show details for a single Mailgun domain.'
@@ -146,7 +181,59 @@ if __name__ == '__main__':
     )
     show_domain.set_defaults(_func=_show_domain)
 
+    # `melon create-route`
+    create_route = subparsers.add_parser(
+        'create-route',
+        help='Create a mailgun route.'
+    )
+    _mailgun_args(create_route)
+    create_route.add_argument(
+        'domain',
+        help='Create a route for mail sent to this domain.'
+    )
+    create_route.add_argument(
+        '--recipient',
+        default='.*',
+        help=('Forward mails matching this recipient expression. '
+              'Defaults to ".*" which matches ANY recipients on the '
+              'domain.'),
+    )
+    create_route.add_argument(
+        '--forward', '-f', nargs='+',
+        help=('Email address or location to forward email to. '
+              'This argument may be specified multiple times to '
+              'forward to multiple locations.')
+    )
+    create_route.add_argument(
+        '--description', '-d',
+        help='Route description',
+        default=('Forward all mailgun domain email '
+                 'to my specified forwarding location(s).')
+    )
+    create_route.add_argument(
+        '--priority', '-p',
+        type=int,
+        # Setting this to 1 so that explicit recipient routes
+        # can be added and triggered.
+        default=1,
+    )
+    create_route.add_argument(
+        '--with-stop', '-s',
+        action='store_true',
+        default=False,
+    )
 
+    create_route.set_defaults(_func=_create_route)
+
+    # `melon show-routes`
+    show_routes = subparsers.add_parser(
+        'show-routes',
+        help='Show routes for a single Mailgun domain.'
+    )
+    _mailgun_args(show_routes)
+    show_routes.set_defaults(_func=_show_routes)
+
+    # `melon tfvars`
     tfvars = subparsers.add_parser(
         'tfvars',
         help='Produce tfvars for Route 53 setup.',
@@ -158,6 +245,7 @@ if __name__ == '__main__':
         help='Domain to get tfvars for.'
     )
 
+    # `melon tf-remote-config`
     tf_remote_config = subparsers.add_parser(
         'tf-remote-config',
         help=('Configure the use of remote state '

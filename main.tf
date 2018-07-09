c3a1cae85e85fade92d7d5916be971baa8c67135
Sam Stavinoha <smlstvnh@gmail.com>
2018-07-08 18:35:15 -0700

    add validation CNAME for cert

--- main.tf
+++ main.tf
@@ -92,3 +92,11 @@ resource "aws_acm_certificate" "stav-dot-xyz-wildcard" {
   }
 }
 
+resource "aws_route53_record" "stav-dot-xyz-acm_validation" {
+  name = "${aws_acm_certificate.stav-dot-xyz-wildcard.domain_validation_options.0.resource_record_name}"
+  type = "${aws_acm_certificate.stav-dot-xyz-wildcard.domain_validation_options.0.resource_record_type}"
+  zone_id = "${aws_route53_zone.stav-dot-xyz.zone_id}"
+  records = ["${aws_acm_certificate.stav-dot-xyz-wildcard.domain_validation_options.0.resource_record_value}"]
+  ttl = 60
+}
+
